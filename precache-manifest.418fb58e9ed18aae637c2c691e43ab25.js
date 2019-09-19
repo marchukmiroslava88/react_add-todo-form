ca7243fdfa89ccae2ccffbf9191beeb143f8e4ff
Miroslava <777shadowpriest777@gmail.com>
2019-09-19 09:25:29 +0300

    Updates

---
+++ precache-manifest.418fb58e9ed18aae637c2c691e43ab25.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6954c89d110551867b327528b0078a2a",
+    "url": "/marchukmiroslava88/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "7eddc3acc40d249e817e",
+    "url": "/marchukmiroslava88/react_add-todo-form/static/css/main.92b51ff6.chunk.css"
+  },
+  {
+    "revision": "bda57ad2dd67ce0ea9d9",
+    "url": "/marchukmiroslava88/react_add-todo-form/static/js/2.ddaedf44.chunk.js"
+  },
+  {
+    "revision": "7eddc3acc40d249e817e",
+    "url": "/marchukmiroslava88/react_add-todo-form/static/js/main.e5570c15.chunk.js"
+  },
+  {
+    "revision": "388f107b2c0f9f232fd0",
+    "url": "/marchukmiroslava88/react_add-todo-form/static/js/runtime~main.7946dd43.js"
+  }
+]);
